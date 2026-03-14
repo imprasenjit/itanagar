68469aaae3b58025d7e0e00e94aa2a6493ba2866
prasenjit <info@netrotechnologies.com>
2026-03-15 00:01:31 +0530

    typescript

---
+++ application/logs/log-2026-03-15.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2026-03-15 00:01:05 --> Config Class Initialized
+INFO - 2026-03-15 00:01:05 --> Hooks Class Initialized
+DEBUG - 2026-03-15 00:01:05 --> UTF-8 Support Enabled
+INFO - 2026-03-15 00:01:05 --> Utf8 Class Initialized
+INFO - 2026-03-15 00:01:05 --> URI Class Initialized
+INFO - 2026-03-15 00:01:05 --> Router Class Initialized
+INFO - 2026-03-15 00:01:05 --> Output Class Initialized
+INFO - 2026-03-15 00:01:05 --> Security Class Initialized
+DEBUG - 2026-03-15 00:01:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-03-15 00:01:05 --> Input Class Initialized
+INFO - 2026-03-15 00:01:05 --> Language Class Initialized
+INFO - 2026-03-15 00:01:05 --> Loader Class Initialized
+INFO - 2026-03-15 00:01:05 --> Helper loaded: url_helper
+INFO - 2026-03-15 00:01:05 --> Helper loaded: file_helper
+INFO - 2026-03-15 00:01:05 --> Helper loaded: cias_helper
+INFO - 2026-03-15 00:01:05 --> Database Driver Class Initialized
+DEBUG - 2026-03-15 00:01:05 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2026-03-15 00:01:05 --> Session: Class initialized using 'files' driver.
+INFO - 2026-03-15 00:01:05 --> Model "User_model" initialized
+INFO - 2026-03-15 00:01:05 --> Controller Class Initialized
+INFO - 2026-03-15 00:01:05 --> Model "Web_model" initialized
+INFO - 2026-03-15 00:01:05 --> Model "Login_model" initialized
+INFO - 2026-03-15 00:01:05 --> Helper loaded: form_helper
+INFO - 2026-03-15 00:01:05 --> Form Validation Class Initialized
